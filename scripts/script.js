292ba0fc29ed6f22c42f56a0dc1d2fa8888b19ae
monokutho <monokutho@gmail.com>
2016-07-20 23:58:14 +0200

    Intro terminada v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,37 @@
-$(document).ready(function (){
-    $('.icon-off').click(function() {
-        $('.icon-on').css('display','inline');
-        $('#contactInfo').css('color','white');
-        $(this).css('display','none');
-        $('#title').fadeOut('fast', function() {
-            $('#sinopsis').fadeIn('fast');
-        });
-        $('#one').css('backgroundImage','url(/img/cv-intro_bnw.jpg)');
+$(document).ready(function () {
+
+//Intro Switch Animations
+$('.icon-off').click(function () {
+    $(this).css('display', 'none');
+    $('.icon-on').css('display', 'inline');
+    $('#contactInfo').css('color', 'white');
+    $('#title').fadeOut('fast', function () {
+        $('#sinopsis').fadeIn('fast');
     });
-    $('.icon-on').click(function() {
-        $('.icon-off').css('display','inline');
-        $('#contactInfo').css('color','transparent');
-        $(this).css('display','none');
-        $('#sinopsis').fadeOut('fast', function() {
-            $('#title').fadeIn('fast');
-        });
-        $('#one').css('backgroundImage','url(/img/cv-intro.jpg)')
+    $('#one').css('backgroundImage', 'url(/img/cv-intro_bnw.jpg)');
+    $('#firstPageDown').css('display', 'inline');
+});
+$('.icon-on').click(function () {
+    $(this).css('display', 'none');
+    $('.icon-off').css('display', 'inline');
+    $('#contactInfo').css('color', 'transparent');
+    $('#sinopsis').fadeOut('fast', function () {
+        $('#title').fadeIn('fast');
     });
+    $('#one').css('backgroundImage', 'url(/img/cv-intro.jpg)')
+    $('#firstPageDown').css('display', 'none');
+});
+
+//Intro - Work Experience Transit
+$('#firstPageDown').click(function () {
+    $('#two').css('display', 'inline');
+    $('html, body').animate({
+        scrollTop: $("#job1").offset().top
+    }, 1000, function() {
+        $('#jobTitle1, #jobImg1, #jobData1').animate({opacity: 1}, 300);
+        })
+    });
+    
+
+
 });
